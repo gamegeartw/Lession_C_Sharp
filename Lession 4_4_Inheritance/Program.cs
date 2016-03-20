eedecaef29f85afa414db0bc4353140e32453cad
gamegear <gamegear.tw@gmail.com>
2016-03-20 10:53:14 +0800

    Add Lession 4-4

---
+++ Lession 4_4_Inheritance/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lession_4_4_Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat();    
+            cat.Name = "cat";
+            cat.Years = 1;
+            Console.WriteLine(cat.Shut);
+
+
+            Dog dog = new Dog();
+            Console.WriteLine(dog.Shut);
+            Console.ReadLine();
+
+        }
+    }
+
+    public class Animals    //Father Class
+    {
+        public int Years { get; set; }
+        public string Name { get; set; }
+
+    }
+
+    public class Cat:Animals    //son Class
+    {
+        public string Shut { get; set; }
+
+        public Cat()
+        {
+            Shut = "meow";
+        }
+    }
+
+    public class Dog : Animals  //son Class
+    {
+        public string Shut { get; set; }
+        public Dog()
+        {
+            this.Name = "dog";  
+            this.Years = 2;
+            Shut = "Wow";
+        }
+    }
+
+}
