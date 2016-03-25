3721cf8dbfe1986841b6771c331b7705dad589d3
gamegear <gamegear.tw@gmail.com>
2016-03-25 21:29:08 +0800

    add lesson 4_4

---
+++ Lession_4_4_Class/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lession_4_4_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SaveTime time = new SaveTime();
+            time.today = DateTime.Now;
+            Console.WriteLine("被儲存起來的時間:"+ time.today);
+            People Kevin = new People();
+            People Wilber = new People();
+            Kevin.Job = "老板";
+            Kevin.Car = "BMW";
+
+            Wilber.Job = "攻城獅";
+            Wilber.Car = "March";
+
+            Console.WriteLine("Kevin的職業是" + Kevin.Job + ",開的車是" + Kevin.Car);
+            Console.WriteLine("Wilber的職業是" + Wilber.Job + ",開的車是" + Wilber.Car);
+            Console.WriteLine("剛剛被儲起來的時間是" +time.today);
+            Console.WriteLine("現在的時間是" + DateTime.Now);
+
+        }
+    }
+
+    public class SaveTime
+    {
+        public DateTime today;
+    }
+
+    public class People
+    {
+        public string Job;
+        public string Car;
+    }
+}
