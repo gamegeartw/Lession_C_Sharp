bc79d4f15998ac4edf62371e041f188c01dc10bc
gamegear <gamegear.tw@gmail.com>
2016-03-25 22:39:52 +0800

    add class2

---
+++ Lession_4_Class2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lession_4_Class2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            People allen = new People();
+            People bob = new People();
+            allen.Name = "王小明";
+            allen.Birthday = new DateTime(1980, 3, 3);
+            allen.LoveTaiwan = true;
+            bob.Name = "李大同";
+            bob.Birthday = new DateTime(1990, 1, 1);
+            bob.LoveTaiwan = false;
+
+            Console.WriteLine(allen.Age());
+            Console.WriteLine(bob.Age());
+            allen.sayLove();
+            bob.sayLove();
+            Console.ReadLine();
+
+        }
+    }
+
+    class People
+    {
+        public string Name;
+
+        public DateTime Birthday;
+
+        public bool LoveTaiwan;
+
+        public int Age()
+        {
+            int yearNow = DateTime.Now.Year;
+            int RealAge = yearNow - Birthday.Year;
+            return RealAge;
+        }
+
+        public void sayLove()
+        {
+            if (LoveTaiwan)
+            {
+                Console.WriteLine("我愛台灣");
+            }else
+            {
+                Console.WriteLine("喔!你不愛台灣");
+            }
+        }
+    }
+}
